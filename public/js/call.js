be78b67d67340ca3781ab9aef08586dc668c1de4
andyhe789 <andyhe987@gmail.com>
2023-04-26 22:59:35 -0700

    display now show on every button press

--- public/js/call.js
+++ public/js/call.js
@@ -1,5 +1,7 @@
 const numButton = document.querySelectorAll('.numPad')
 const callBut = document.querySelectorAll("#callButton")
+const displayScreen = document.querySelector('#display')
+let text = ''
 
 numButton.forEach((el)=>{
     el.addEventListener('click', printNum)
@@ -10,8 +12,10 @@ callBut.forEach((el)=>{
 })
 
 function printNum(){
-    const contactID = this.dataset.number
-    console.log(contactID)
+    const contactText = this.dataset.number
+    text += contactText
+    console.log(text)
+    displayScreen.value = text
 }
 function calling(){
     console.log('callllll')
