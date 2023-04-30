c40240e47affd8bf547d6ca6f65b3d79a27610d9
andyhe789 <andyhe987@gmail.com>
2023-04-30 01:40:31 -0700

    added more css to call page

--- public/js/call.js
+++ public/js/call.js
@@ -1,6 +1,7 @@
 const numButton = document.querySelectorAll('.numPad')
 const callBut = document.querySelectorAll("#callButton")
 const displayScreen = document.querySelector('#display')
+const backspaceBut = document.querySelector('#backspace')
 let text = ''
 
 numButton.forEach((el)=>{
@@ -11,12 +12,24 @@ callBut.forEach((el)=>{
     el.addEventListener('click', calling)
 })
 
+backspaceBut.addEventListener('click', backspaceOne)
+
 function printNum(){
     const contactText = this.dataset.number
     text += contactText
     console.log(text)
     displayScreen.value = text
+    return text
 }
+
+function backspaceOne(text){
+    console.log(text)
+}
+
 function calling(){
     console.log('callllll')
+}
+
+function test(text){
+    console.log(text)
 }
