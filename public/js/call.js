35336c7b6b58d6b9299c6bbaccf53ed55dc28808
andyhe789 <andyhe987@gmail.com>
2023-05-01 00:26:56 -0700

    using classes for phone instead of mutiple functions

--- public/js/call.js
+++ public/js/call.js
@@ -1,35 +1,34 @@
+class Phone{
+    constructor(displayScreen){
+        this.displayScreen = displayScreen
+    }
+
+    clear(){
+        this.displayText = ''
+    }
+
+    appendNumber(number){
+        this.displayText = this.displayText.toString() + number.toString()
+        
+    }
+
+    updateDisplay(){
+        this.displayScreen.value += this.displayText
+    }
+}
+
+
 const numButton = document.querySelectorAll('.numPad')
 const callBut = document.querySelectorAll("#callButton")
 const displayScreen = document.querySelector('#display')
 const backspaceBut = document.querySelector('#backspace')
-let text = ''
+
+const phone = new Phone(displayScreen)
 
 numButton.forEach((el)=>{
-    el.addEventListener('click', printNum)
+    el.addEventListener('click', ()=>{
+        phone.appendNumber(el.innerText)
+        phone.updateDisplay()
+    })
 })
 
-callBut.forEach((el)=>{
-    el.addEventListener('click', calling)
-})
-
-backspaceBut.addEventListener('click', backspaceOne)
-
-function printNum(){
-    const contactText = this.dataset.number
-    text += contactText
-    console.log(text)
-    displayScreen.value = text
-    return text
-}
-
-function backspaceOne(text){
-    console.log(text)
-}
-
-function calling(){
-    console.log('callllll')
-}
-
-function test(text){
-    console.log(text)
-}
