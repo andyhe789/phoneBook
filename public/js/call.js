ddcee4aab0560061fe142f8d953b74fcff410124
andyhe789 <andyhe987@gmail.com>
2023-06-21 00:26:09 -0700

    added () phone # format

--- public/js/call.js
+++ public/js/call.js
@@ -6,9 +6,9 @@ class Phone{
 
     clear(){
         this.displayText = this.displayText.toString().slice(0,-1)
-        if(this.displayLength == 4){
-            this.displayText = this.displayText.toString().slice(0,-1)
-        }
+        // if(this.displayLength == 5){
+        //     this.displayText = this.displayText.replace('-','')
+        // }
     }
 
     appendNumber(number){
@@ -17,14 +17,24 @@ class Phone{
 
     phoneFormat(){
         this.displayLength = this.displayText.toString().length
+        let specialCharacters = '( ) -'
         console.log(this.displayLength)
-        if(this.displayLength > 4 && this.displayLength < 8 && this.displayText.includes('-') === false){
-            this.displayText = this.displayText.slice(0,3) + '-' + this.displayText.slice(3,6)
-            console.log(this.displayText.includes('-'))
+        if(this.displayLength <= 3){
+            this.displayText = this.displayText.slice(0,3)
+            
         }
-        // else if(this.displayLength >= 8){
-        //     this.displayText = '(' + this.displayText.slice(0,3) +')' + this.displayText.slice(3,6) + '-' 
-        // }
+        if(this.displayLength == 3 && this.displayText.includes('-') === true){
+            this.displayText = this.displayText.replace('-','')
+        }
+        if(this.displayLength > 3 && this.displayLength < 8 && this.displayText.includes('-') === false){
+            this.displayText = this.displayText.slice(0,3) + '-' + this.displayText.slice(3,6)
+            console.log('run')
+        }
+        if(this.displayLength >= 8 && this.displayText.includes('(') === false){
+            this.displayText = this.displayText.replace('-','')
+            this.displayText = '(' + this.displayText.slice(0,3) +')' + this.displayText.slice(3,6) + '-' + this.displayText.slice(6)
+            console.log('as')
+        }
     }
     
     updateDisplay(){
