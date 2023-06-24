9b3f628017342216116b7ee1dc5b3080b68f16a7
andyhe789 <andyhe987@gmail.com>
2023-06-24 01:00:35 -0700

    backspace not working as intended

--- public/js/call.js
+++ public/js/call.js
@@ -2,6 +2,7 @@ class Phone{
     constructor(displayScreen){
         this.displayScreen = displayScreen
         this.displayText = ''
+        this.displayLength = ''
     }
 
     clear(){
@@ -16,27 +17,45 @@ class Phone{
     }
 
     phoneFormat(){
-        this.displayLength = this.displayText.toString().length
+        this.displayLength = this.displayText.toString().replace(/[-()]/gi,'').length
         let specialCharacters = '( ) -'
         console.log(this.displayLength)
         if(this.displayLength <= 3){
             this.displayText = this.displayText.slice(0,3)
-            
         }
-        if(this.displayLength == 3 && this.displayText.includes('-') === true){
-            this.displayText = this.displayText.replace('-','')
-        }
-        if(this.displayLength > 3 && this.displayLength < 8 && this.displayText.includes('-') === false){
+        if(this.displayLength == 3 && this.displayLength < 8 && this.displayText.includes('-') === false){
             this.displayText = this.displayText.slice(0,3) + '-' + this.displayText.slice(3,6)
             console.log('run')
         }
-        if(this.displayLength >= 8 && this.displayText.includes('(') === false){
+        if(this.displayLength >= 8 && this.displayLength <= 11 && this.displayText.includes('(') === false){
             this.displayText = this.displayText.replace('-','')
             this.displayText = '(' + this.displayText.slice(0,3) +')' + this.displayText.slice(3,6) + '-' + this.displayText.slice(6)
             console.log('as')
         }
+        if(this.displayLength >= 10){
+            this.displayText = this.displayText.replace(/[-()]/gi,'')
+            console.log(this.displayLength)
+        }
     }
     
+    backspaceFormat(){
+        this.displayLength = this.displayText.toString().replace(/[-()]/gi,'').length
+        if(this.displayLength <= 7){
+            this.displayText = this.displayText.replace(/[()-]/gi,'')
+            this.displayText = this.displayText.split('')
+            // this.displayText = this.displayText.slice(0,3) + '-' + this.displayText.slice(3,6)
+            console.log(this.displayLength)
+        }
+        console.log(' dele')
+        // if(this.displayLength >= 8 && this.displayLength <= 11 && this.displayText.includes('(') === false){
+        //     console.log(this.displayLength)
+        //     console.log('deas')
+        // }
+        // if(this.displayLength <= 7){
+        //     this.displayText = this.displayText.replace(/[()]/gi,'')
+        // }
+    }
+
     updateDisplay(){
         this.displayScreen.value = this.displayText
     }
@@ -54,14 +73,16 @@ const phone = new Phone(displayScreen)
 numButton.forEach((el)=>{
     el.addEventListener('click', ()=>{
         phone.appendNumber(el.innerText)
-        phone.updateDisplay()
         phone.phoneFormat()
+        phone.updateDisplay()
+        
     })
 })
 
 backButton.addEventListener('click', ()=>{
     phone.clear()
+    phone.backspaceFormat()
     phone.updateDisplay()
-    phone.phoneFormat()
+    // phone.phoneFormat()
 })
 
